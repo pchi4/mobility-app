fb9bb061b839ce81cb018cbca65911a171c51f6c
Douglas Lopes <pchialoopes@gmail.com>
2025-11-13 15:25:58 -0300

    feat: Remove Mobility App Scret key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDvNypCJVAfgPJ1nmrqZvz25wSbW3JOjUc")
+    GMSServices.provideAPIKey("")
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
